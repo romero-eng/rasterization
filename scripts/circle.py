9c7ecb0589eb608dc0763aee15b0ef2b4bd6a5ec
romero-eng <39012010+romero-eng@users.noreply.github.com>
2025-04-03 14:41:35 -0400

    Simplified circle rasterization

--- scripts/circle.py
+++ scripts/circle.py
@@ -6,7 +6,8 @@ import argparse
 from typing import Any
 
 
-def py_impl_circle(radius: int) -> np.ndarray[tuple[int, int], np.dtype[Any]]:
+def py_impl_circle(radius: int,
+                   center: tuple[int, int]) -> np.ndarray[tuple[int, int], np.dtype[Any]]:
 
     N: int = int(np.floor(radius/np.sqrt(2))) + 1
     tau: int = 4*np.square(radius) - 5
@@ -24,42 +25,109 @@ def py_impl_circle(radius: int) -> np.ndarray[tuple[int, int], np.dtype[Any]]:
             (first_octant_points[n][0] - (1 if decrement else 0),
              first_octant_points[n][1] + 1)
 
-    Q: int = 2*N - (1 if first_octant_points[N - 1][0] == first_octant_points[N - 1][1] else 0)
-    T: int = 2*Q - 1
-    C: int = 2*(T - 1)
+    overflow: int = 1 if first_octant_points[N - 1][0] == first_octant_points[N - 1][1] else 0
+    M: int = N - overflow
+    Q: int = 2*N - 1 - overflow
 
-    circular_arc_points: np.ndarray[tuple[int, int], np.dtype[Any]] = np.empty((C, 2), dtype=int)
+    points: np.ndarray[tuple[int, int], np.dtype[Any]] = np.empty((4*Q, 2), dtype=int)
 
-    for n in range(0, N):
-        circular_arc_points[n] = first_octant_points[n]
+    # Right-most point
+    points[0] = \
+        (first_octant_points[0][0] + center[0],
+         first_octant_points[0][1] + center[1])
 
-    for n in range(N, Q):
-        circular_arc_points[n] = \
-            (first_octant_points[Q - n - 1][1],
-             first_octant_points[Q - n - 1][0])
+    # Upper-most point
+    points[Q] = \
+        (first_octant_points[0][1] + center[0],
+         first_octant_points[0][0] + center[1])
 
-    for q in range(Q, T):
-        circular_arc_points[q] = \
-            (-circular_arc_points[T - 1 - q][0],
-              circular_arc_points[T - 1 - q][1])  # noqa: E127
+    # Left-most point
+    points[2*Q] = \
+        (-first_octant_points[0][0] + center[0],
+          first_octant_points[0][1] + center[1])
 
-    for t in range(T, C):
-        circular_arc_points[t] = \
-            ( circular_arc_points[C - t][0],  # noqa: E201
-             -circular_arc_points[C - t][1])  # noqa: E128
+    # Bottom-most point
+    points[3*Q] = \
+        (-first_octant_points[0][1] + center[0],
+         -first_octant_points[0][0] + center[1])
 
-    return circular_arc_points
+    if overflow:
+
+        # Upper-left diagonal point
+        points[N - 1] = \
+            (first_octant_points[N - 1][0] + center[0],
+             first_octant_points[N - 1][1] + center[1])
+
+        # Upper-right diagonal point
+        points[2*Q - (N - 1)] = \
+            (-first_octant_points[N - 1][0] + center[0],
+              first_octant_points[N - 1][1] + center[1])
+
+        # Lower-right diagonal point
+        points[2*Q + (N - 1)] = \
+            (-first_octant_points[N - 1][0] + center[0],
+             -first_octant_points[N - 1][1] + center[1])
+
+        # Lower-left diagonal point
+        points[4*Q - (N - 1)] = \
+            ( first_octant_points[N - 1][0] + center[0],
+             -first_octant_points[N - 1][1] + center[1])
+
+
+    for m in range(1, M):
+
+        # First Octant
+        points[m] = \
+            (first_octant_points[m][0] + center[0],
+             first_octant_points[m][1] + center[1])
+
+        # Second Octant
+        points[Q - m] = \
+            (first_octant_points[m][1] + center[0],
+             first_octant_points[m][0] + center[1])
+
+        # Third Octant
+        points[Q + m] = \
+            (-first_octant_points[m][1] + center[0],
+              first_octant_points[m][0] + center[1])
+        # Fourth Octant
+        points[2*Q - m] = \
+            (-first_octant_points[m][0] + center[0],
+              first_octant_points[m][1] + center[1])
+
+        # Fifth Octant
+        points[2*Q + m] = \
+            (-first_octant_points[m][0] + center[0],
+             -first_octant_points[m][1] + center[1])
+
+        # Sixth Octant
+        points[3*Q - m] = \
+            (-first_octant_points[m][1] + center[0],
+             -first_octant_points[m][0] + center[1])
+
+        # Seventh Octant
+        points[3*Q + m] = \
+            ( first_octant_points[m][1] + center[0],
+             -first_octant_points[m][0] + center[1])
+
+        # Eighth Octant
+        points[4*Q - m] = \
+            ( first_octant_points[m][0] + center[0],
+             -first_octant_points[m][1] + center[1])
+
+    return points
 
 
 def plot_circle_rasterization(radius: int,
+                              center: tuple[int, int],
                               x_step: float,
                               x_inches: int,
                               y_inches: int,
                               dpi: int) -> None:
 
-    x: np.ndarray[tuple[int], np.dtype[Any]] = np.arange(-radius, radius + x_step, x_step)
-    top_half: np.ndarray[tuple[int], np.dtype[Any]] = np.sqrt(radius**2 - x**2)
-    bottom_half: np.ndarray[tuple[int], np.dtype[Any]] = -top_half
+    x: np.ndarray[tuple[int], np.dtype[Any]] = np.arange(-radius, radius + x_step, x_step) + center[0]
+    top_half: np.ndarray[tuple[int], np.dtype[Any]] = np.sqrt(radius**2 - (x - center[0])**2) + center[1]
+    bottom_half: np.ndarray[tuple[int], np.dtype[Any]] = -np.sqrt(radius**2 - (x - center[0])**2) + center[1]
 
     fig, ax = plt.subplots()
     fig.set_size_inches(x_inches, y_inches)
@@ -68,37 +136,39 @@ def plot_circle_rasterization(radius: int,
     ax.plot(x,    top_half)
     ax.plot(x, bottom_half)
 
-    points: np.ndarray[tuple[int, int], np.dtype[Any]] = py_impl_circle(radius)
-
+    points: np.ndarray[tuple[int, int], np.dtype[Any]] = py_impl_circle(radius, center)
+ 
     for i in range(0, len(points)):
         print("({X: 2d}, {Y: 2d}), {angle:.2f}".format(X=points[i][0],
                                                        Y=points[i][1],
-                                                       angle=(180/np.pi)*np.arctan2(points[i][1],
-                                                                                    points[i][0])))
+                                                       angle=(180/np.pi)*np.arctan2(points[i][1] - center[1],
+                                                                                    points[i][0] - center[0])))
 
     for i in range(0, len(points) - 1):
         ax.plot([points[i][0], points[i + 1][0]],
                 [points[i][1], points[i + 1][1]],
                 marker="x", color="black")
 
-    ax.set_xticks(np.arange(-radius - 2, radius + 3, 1))
-    ax.set_yticks(np.arange(-radius - 2, radius + 3, 1))
+    ax.set_xticks(np.arange(-radius - 2 + center[0], radius + 3 + center[0], 1))
+    ax.set_yticks(np.arange(-radius - 2 + center[1], radius + 3 + center[1], 1))
     ax.set_xlabel("X")
     ax.set_ylabel("Y", rotation=0)
-    ax.set_xlim((-radius - 2, radius + 2))
-    ax.set_ylim((-radius - 2, radius + 2))
+    ax.set_xlim((-radius - 2 + center[0], radius + 2 + center[0]))
+    ax.set_ylim((-radius - 2 + center[1], radius + 2 + center[1]))
     ax.set_title("Circle")
     ax.set_aspect("equal")
     ax.grid()
 
-    ax.axhline(0)
-    ax.axvline(0)
+    ax.axhline(center[1])
+    ax.axvline(center[0])
 
     fig.tight_layout()
 
     fig.savefig("circle.png")
     subprocess.run(["feh", "circle.png"])
     os.remove("circle.png")
+
+
 
 
 if (__name__ == "__main__"):
@@ -108,6 +178,8 @@ if (__name__ == "__main__"):
                                 description="Visualization of Circle Rasterization Algorithm")
 
     parser.add_argument("radius", type=int)
+    parser.add_argument("X_center", type=int)
+    parser.add_argument("Y_center", type=int)
     parser.add_argument("x_step", type=float)
     parser.add_argument("X_inches", type=int)
     parser.add_argument("Y_inches", type=int)
@@ -115,6 +187,8 @@ if (__name__ == "__main__"):
     args: argparse.Namespace = parser.parse_args()
 
     plot_circle_rasterization(args.radius,
+                              (args.X_center,
+                               args.Y_center),
                               args.x_step,
                               args.X_inches,
                               args.Y_inches,
