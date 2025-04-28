19cb884cf29fd08ee8c3d43e9d52d8f72c44d216
romero-eng <39012010+romero-eng@users.noreply.github.com>
2025-04-28 14:11:45 -0400

    Nested shell command functions
    Also, Flake8 optimizations

--- scripts/build.py
+++ scripts/build.py
@@ -9,45 +9,6 @@ from pathlib import Path
 from enum import Enum, IntEnum
 
 
-def mapping_to_string(mapping_title: str,
-                      mapping: dict[str, str]) -> str:
-
-    max_key_length: int = max([len(key) for key in mapping])
-    return f'\n{mapping_title:s}:\n{'':{'-':s}>{len(mapping_title) + 1:d}s}\n{'\n'.join([f'  {key:>{max_key_length:d}s}: {value:s}' for key, value in mapping.items()]):s}\n'  # noqa: E231, E501
-
-
-def run_shell_command(description: str,
-                      command: str,
-                      shell_path: Path = Path.cwd(),
-                      successful_return_code: int = 0) -> None:
-
-    shell_results: subprocess.CompletedProcess[bytes] = \
-        subprocess.run(command,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE,
-                       cwd=shell_path,
-                       shell=True)
-
-    command_results: dict[str, str] = \
-        {'Directory': str(shell_path),
-           'command': command}             # noqa: E127
-
-    if shell_results.stdout:
-        command_results['Output'] = f'\n\n{'\n'.join([f'{line:s}' for line in shell_results.stdout.decode('utf-8').split('\n')]):s}'  # noqa: E501
-
-    if shell_results.stderr:
-        command_results['Error'] = f'\n\n{'\n'.join([f'{line:s}' for line in shell_results.stderr.decode('utf-8').split('\n')]):s}'  # noqa: E501
-
-    printable_shell_results: str = \
-        mapping_to_string(description,
-                          command_results)
-
-    if shell_results.returncode == successful_return_code:
-        print(printable_shell_results)
-    else:
-        raise Exception(f'\n{printable_shell_results:s}')
-
-
 class Optimization(IntEnum):
 
     NONE = 0
@@ -82,7 +43,49 @@ class Output(Enum):
     PYTHON = OutputType("g++ {{object_files:s}} -shared -o {dynamic_library:s}.so {{library_names:s}}", "py")
 
 
-class Batch:
+class CMD_Runner:
+
+    def _mapping_to_string(self,
+                           mapping_title: str,
+                           mapping: dict[str, str]) -> str:
+
+        max_key_length: int = max([len(key) for key in mapping])
+        return f'\n{mapping_title:s}:\n{'':{'-':s}>{len(mapping_title) + 1:d}s}\n{'\n'.join([f'  {key:>{max_key_length:d}s}: {value:s}' for key, value in mapping.items()]):s}\n'  # noqa: E231, E501
+
+    def _run_shell_command(self,
+                           description: str,
+                           command: str,
+                           shell_path: Path = Path.cwd(),
+                           successful_return_code: int = 0) -> None:
+
+        shell_results: subprocess.CompletedProcess[bytes] = \
+            subprocess.run(command,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           cwd=shell_path,
+                           shell=True)
+
+        command_results: dict[str, str] = \
+            {'Directory': str(shell_path),
+               'command': command}             # noqa: E127
+
+        if shell_results.stdout:
+            command_results['Output'] = f'\n\n{'\n'.join([f'{line:s}' for line in shell_results.stdout.decode('utf-8').split('\n')]):s}'  # noqa: E501
+
+        if shell_results.stderr:
+            command_results['Error'] = f'\n\n{'\n'.join([f'{line:s}' for line in shell_results.stderr.decode('utf-8').split('\n')]):s}'  # noqa: E501
+
+        printable_shell_results: str = \
+            self._mapping_to_string(description,
+                                    command_results)
+
+        if shell_results.returncode == successful_return_code:
+            print(printable_shell_results)
+        else:
+            raise Exception(f'\n{printable_shell_results:s}')
+
+
+class Batch(CMD_Runner):
 
     _cpp_versions = ["0x", "1y", "1z", "2a", "2b"]
 
@@ -164,11 +167,11 @@ class Batch:
 
         if hasattr(self, "_remove_asserts"):
             if self._remove_asserts and decision:
-                print("Warning: removing assert statements is not recommended when low-level assembly debugging is enabled")
+                print("Warning: removing assert statements is not recommended when low-level assembly debugging is enabled")  # noqa: E501
 
         if hasattr(self, "_optimization_level"):
             if self._optimization_level is not Optimization.NONE and decision:
-                print("Warning: enabling optimizations will result in discrepancies between assembly code and C\\C++ source code.")
+                print("Warning: enabling optimizations will result in discrepancies between assembly code and C\\C++ source code.")  # noqa: E501
 
         self._assembly_debugging = decision
 
@@ -176,7 +179,7 @@ class Batch:
 
         if hasattr(self, "_assembly_debugging"):
             if self._assembly_debugging and level is not Optimization.NONE:
-                print("Warning: enabling optimizations will result in discrepancies between assembly code and C\\C++ source code.")
+                print("Warning: enabling optimizations will result in discrepancies between assembly code and C\\C++ source code.")  # noqa: E501
 
         self._optimization_level: Optimization = level
 
@@ -184,7 +187,7 @@ class Batch:
 
         if hasattr(self, "_assembly_debbugging"):
             if self._assembly_debugging and decision:
-                print("Warning: removing assert statements is not recommended when low-level assembly debugging is enabled")
+                print("Warning: removing assert statements is not recommended when low-level assembly debugging is enabled")  # noqa: E501
 
         self._remove_asserts: bool = decision
 
@@ -227,8 +230,8 @@ class Batch:
     def _yield_formatted_flags(self) -> str:
 
         main_decisions: dict[str, str | None] = \
-            {                           "std": f"c++{self.__class__._cpp_versions[int((self._version_year - 2011)/3)]:2s}",  # noqa: E201
-             f"O{self._optimization_level:d}": None}                                                                         # noqa: E128
+            {                           "std": f"c++{self.__class__._cpp_versions[int((self._version_year - 2011)/3)]:2s}",  # noqa: E201, E501
+             f"O{self._optimization_level:d}": None}                                                                         # noqa: E128, E501
 
         if self._disable_compiler_extensions:
             main_decisions |= {"pedantic-errors": None}
@@ -272,7 +275,7 @@ class Batch:
             [f"{flag:s}={value:s}" if value is not None else f"{flag:s}" for flag, value in main_decisions.items()] + \
             [f"D{macro:s}={value:s}" if value is not None else f"D{macro:s}" for macro, value in preprocessor_macros.items()] + \
             [f"I {str(include_dir):s}" for include_dir in self._include_dirs] + \
-            [f"W{flag:s}" for flag in warnings]
+            [f"W{flag:s}" for flag in warnings]  # noqa: E501
 
         return " ".join([f"-{flag:s}" for flag in flags])
 
@@ -294,15 +297,15 @@ class Batch:
                     abs_obj_file = (build_dir/rel_src_file.name).with_suffix(".o")
                     abs_obj_files.append(abs_obj_file)
 
-                    run_shell_command(f"Compile {rel_src_file.name:s}",
-                                      compilation_template.format(source_file=str(rel_src_file),
-                                                                  object_file=str(abs_obj_file.relative_to(common_dir))),
-                                      shell_path=common_dir)
+                    self._run_shell_command(f"Compile {rel_src_file.name:s}",
+                                            compilation_template.format(source_file=str(rel_src_file),
+                                                                        object_file=str(abs_obj_file.relative_to(common_dir))),  # noqa: E501
+                                            shell_path=common_dir)
 
         return abs_obj_files
 
 
-class Builder:
+class Builder(CMD_Runner):
 
     def __init__(self,
                  build_dir: Path,
@@ -320,16 +323,17 @@ class Builder:
                  template_description: str,
                  template: str) -> None:
 
-        abs_obj_files: list[Path] = [abs_obj_file for batch in self._batches for abs_obj_file in batch.compile(self._build_dir)]
+        abs_obj_files: list[Path] = \
+            [abs_obj_file for batch in self._batches for abs_obj_file in batch.compile(self._build_dir)]
 
         unique_library_names: set[str] = set.union(*[batch.yield_library_names() for batch in self._batches])
 
         formatted_library_names: str = " ".join([f"-l{library_name:s}" for library_name in unique_library_names])
 
-        run_shell_command(template_description,
-                          template.format(object_files=" ".join([str(abs_obj_file.relative_to(self._build_dir)) for abs_obj_file in abs_obj_files]),
-                                          library_names=formatted_library_names),
-                          shell_path=self._build_dir)
+        self._run_shell_command(template_description,
+                                template.format(object_files=" ".join([str(abs_obj_file.relative_to(self._build_dir)) for abs_obj_file in abs_obj_files]),  # noqa: E501
+                                                library_names=formatted_library_names),
+                                shell_path=self._build_dir)
 
         for abs_obj_file in abs_obj_files:
             abs_obj_file.unlink()
@@ -363,7 +367,7 @@ class Builder:
                 if prototype_wrapper_script.suffix == ".txt":
                     with open(prototype_wrapper_script, "r") as prototype_wrapper_script_IO:
                         with open(self._build_dir/f"{prototype_wrapper_script.stem:s}.py", "a") as wrapper_script_IO:
-                            wrapper_script_IO.write(prototype_wrapper_script_IO.read().format(library_name=self._output_file_basename))
+                            wrapper_script_IO.write(prototype_wrapper_script_IO.read().format(library_name=self._output_file_basename))  # noqa: E501
 
     def add_batch(self, batch: Batch) -> None:
 
@@ -378,9 +382,9 @@ class Builder:
 
         self._build_executable()
 
-        run_shell_command("Run Executable",
-                          f"./{self._output_file_basename:s}.exe",
-                          self._build_dir)
+        self._run_shell_command("Run Executable",
+                                f"./{self._output_file_basename:s}.exe",
+                                self._build_dir)
 
         shutil.rmtree(self._build_dir)
 
